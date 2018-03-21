421e8acae589cd4d53a78bc775cca76755d554b0
Carlos Ezequiel <cezequiel@google.com>
2018-03-21 15:44:07 -0400

    Add data loading and preprocessing to model module.

--- trainer/model.py
+++ trainer/model.py
@@ -1,6 +1,42 @@
 """Machine learning model definitions."""
 
 import tensorflow as tf
+import keras
+from keras.datasets import mnist
+
+
+IMG_ROWS, IMG_COLS = 28, 28
+IMG_MAX_VAL = 255
+NUM_CLASSES = 10
+INPUT_SHAPE = (IMG_ROWS, IMG_COLS, 1)
+
+
+class MNISTData(object):
+
+  def __init__(self):
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    self.x_train = _preprocess_features(x_train)
+    self.y_train = _preprocess_labels(y_train, NUM_CLASSES)
+    self.x_test = _preprocess_features(x_test)
+    self.y_test = _preprocess_labels(y_test, NUM_CLASSES)
+
+
+data = MNISTData()
+
+
+def _preprocess_features(x):
+  """Preprocess input features."""
+  x = x.reshape(x.shape[0], IMG_ROWS, IMG_COLS, 1)
+  x = x.astype('float32')
+  x /= IMG_MAX_VAL
+  return x
+
+
+def _preprocess_labels(y, num_classes):
+  """Preprocess label vector"""
+
+  # One-hot encode labels based on num_classes
+  return keras.utils.to_categorical(y, num_classes)
 
 
 def make_input_fn(features, labels=None, shuffle=False):
@@ -14,6 +50,14 @@ def make_input_fn(features, labels=None, shuffle=False):
   return input_fn
 
 
+def make_train_input_fn(shuffle=False):
+  return make_input_fn(data.x_train, data.y_train, shuffle)
+
+
+def make_eval_input_fn():
+  return make_input_fn(data.x_test, data.y_test, shuffle=False)
+
+
 def make_json_serving_input_fn(input_shape):
   def input_fn():
     images_input = tf.placeholder(shape=input_shape,
@@ -24,7 +68,7 @@ def make_json_serving_input_fn(input_shape):
   return input_fn
 
 
-def build_estimator(input_shape, num_classes):
+def build_estimator(config, input_shape, num_classes):
   model = tf.keras.models.Sequential()
   model.add(tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu',
                                    input_shape=input_shape, name='images'))
@@ -39,6 +83,6 @@ def build_estimator(input_shape, num_classes):
   model.compile(loss='categorical_crossentropy', optimizer='adadelta',
                 metrics=['accuracy'])
 
-  return tf.keras.estimator.model_to_estimator(keras_model=model)
+  return tf.keras.estimator.model_to_estimator(keras_model=model, config=config)
 
 
